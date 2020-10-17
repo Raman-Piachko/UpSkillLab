b7ee47d26df51ccbc4ecef1e47f6011333c6c4b2
Raman Piachko <rpechko88@gmail.com>
2020-10-17 19:38:15 +0300

    1

--- src/branching/Task4.java
+++ src/branching/Task4.java
@@ -7,7 +7,7 @@ public class Task4 {
     }
 
     public static boolean checkSize(double a, double b, double x, double y, double z) {
-        return ((a * b) > (x * y) || (a * b) > (x * z) || (a * b) > (y * z));
+        return ((a<x)&&(b<y))||((a<x)&&(b<z))||((a<z)&&(b<y));
     }
 
 }
