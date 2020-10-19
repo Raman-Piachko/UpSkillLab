6f7d2fd205444595a4de5a124753286232f92671
Raman Piachko <rpechko88@gmail.com>
2020-10-19 15:01:37 +0300

    for review

--- src/com/epam/task/cycle/Task3.java
+++ src/com/epam/task/cycle/Task3.java
@@ -10,7 +10,7 @@ public class Task3 {
     public static int calculateSummaSqrFirst100Num() {
         int summ = 0;
         for (int i = 0; i <= 100; i++) {
-            summ += pow(i,2);
+            summ += pow(i, 2);
         }
         return summ;
     }
--- src/com/epam/task/cycle/Task5.java
+++ src/com/epam/task/cycle/Task5.java
@@ -4,9 +4,10 @@ public class Task5 {
     public static void main(String[] args) {
         convertNumToChar();
     }
-    public static void convertNumToChar(){
+
+    public static void convertNumToChar() {
         for (int i = 0; i < 255; i++) {
-            System.out.printf("Char - %s == int - %d\n", (char)i, i);
+            System.out.printf("Char - %s == int - %d\n", (char) i, i);
         }
     }
 }
