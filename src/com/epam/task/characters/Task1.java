9640f6bff6e1ad94491ff7e8b0c987b88247ebe7
Raman Piachko <rpechko88@gmail.com>
2020-11-05 21:24:24 +0300

    in progress

---
+++ src/com/epam/task/characters/Task1.java
@@ -0,0 +1,91 @@
+package com.epam.task.characters;
+
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String[] string = {"charArrayAfsTfsfsY", "safAsffF"};
+        String[] formatArrayOfStings = formatArrayOfStings(string);
+        System.out.println(Arrays.toString(formatArrayOfStings));
+    }
+
+    public static String[] formatArrayOfStings(String[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            strings[i] = formatStringToSnakeCase(strings[i]);
+        }
+
+        return strings;
+    }
+
+    public static String formatStringToSnakeCase(String sting) {
+        char[] chars = sting.toCharArray();
+        int[] index = findIndex(chars);
+
+        for (int i = index.length - 1; i >= 0; i--) {
+            for (int j = chars.length - 1; j >= 0; j--) {
+                if (index[i] == j) {
+                    chars = insertCharacter(chars, j, '_');
+                }
+            }
+        }
+        formatToLowerCase(chars);
+        return String.valueOf(chars);
+    }
+
+    public static char[] insertCharacter(char[] chars, int index, char val) {
+
+        char[] dest_Array = new char[chars.length + 1];
+        int j = 0;
+
+        for (int i = 0; i < dest_Array.length; i++) {
+            if (i == index) {
+                dest_Array[i] = val;
+            } else {
+                dest_Array[i] = chars[j];
+                j++;
+            }
+        }
+        System.out.println(Arrays.toString(dest_Array));
+        return dest_Array;
+    }
+
+    public static int countIndex(char[] chars) {
+        int count = 0;
+
+        for (char aChar : chars) {
+            if (Character.isUpperCase(aChar)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int[] findIndex(char[] chars) {
+        int[] index = new int[countIndex(chars)];
+        int step = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isUpperCase(chars[i])) {
+                index[step] = i;
+                step++;
+
+            }
+        }
+
+        return index;
+    }
+
+
+    public static void formatToLowerCase(char[] chars) {
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = Character.toLowerCase(chars[i]);
+        }
+    }
+}
+
+
+
+
+
+
---
+++ src/com/epam/task/characters/Task2.java
@@ -0,0 +1,95 @@
+package com.epam.task.characters;
+
+import static com.epam.task.characters.Task1.insertCharacter;
+
+public class Task2 {
+    public static void main(String[] args) {
+        String string = "word word";
+        System.out.println(changeWordToLetter(string));
+    }
+
+
+    public static int countIndexWord(char[] chars) {
+        int count = 0;
+        for (int i = 0; i < chars.length - 3; i++) {
+            if (chars[i] == 'w' && chars[i + 1] == 'o' && chars[i + 2] == 'r' && chars[i + 3] == 'd') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static int[] findIndexWord(char[] chars) {
+        int[] index = new int[countIndexWord(chars)];
+        int step = 0;
+
+        for (int i = 0; i < chars.length - 3; i++) {
+            if (chars[i] == 'w' && chars[i + 1] == 'o' && chars[i + 2] == 'r' && chars[i + 3] == 'd') {
+                index[step] = i;
+                step++;
+
+            }
+        }
+
+        return index;
+    }
+
+
+
+    public static String formatedStringToE(String sting) {
+        char[] chars = sting.toCharArray();
+        int[] index = findIndexWord(chars);
+
+        for (int i = index.length - 1; i >= 0; i--) {
+            for (int j = chars.length - 1; j >= 0; j--) {
+                if (index[i] == j) {
+                    chars = insertCharacter(chars, j+4,'e');
+                }
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+
+    public static String formatedStringToER(String string) {
+        char[] chars = string.toCharArray();
+        int[] index = findIndexWord(chars);
+
+        for (int i = index.length - 1; i >= 0; i--) {
+            for (int j = chars.length - 1; j >= 0; j--) {
+                if (index[i] == j) {
+                    chars = insertCharacter(chars, j+5,'r');
+                }
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+
+    public static String changeWordToLett(String string) {
+        char[] chars = string.toCharArray();
+
+
+        for (int i = 0; i < chars.length - 4; i++) {
+            if (chars[i] == 'w' && chars[i + 1] == 'o' && chars[i + 2] == 'r' && chars[i + 3] == 'd') {
+                chars[i] = 'l';
+                chars[i + 1] = 'e';
+                chars[i + 2] = 't';
+                chars[i + 3] = 't';
+
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+
+    public static  String changeWordToLetter(String string){
+        string=formatedStringToE(string);
+        string=formatedStringToER(string);
+
+        return changeWordToLett(string);
+    }
+
+
+}
