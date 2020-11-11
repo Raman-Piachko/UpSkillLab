e6ecbea72f710872cb015d78dc90622fc697c75d
Raman Piachko <rpechko88@gmail.com>
2020-11-11 10:36:24 +0300

    done

--- src/com/epam/task/regex/Task1.java
+++ src/com/epam/task/regex/Task1.java
@@ -1,33 +1,40 @@
 package com.epam.task.regex;
 
-import static com.epam.task.service.StringService.*;
-import static com.epam.task.util.InputUtils.inputString;
+import static com.epam.task.service.StringService.sortLexemeInCurrentSentence;
+import static com.epam.task.service.StringService.sortParagraphsByNumberOfSentences;
+import static com.epam.task.service.StringService.sortWordsInSentenceByLength;
+import static com.epam.task.util.InputUtils.inputIntValue;
 
 public class Task1 {
     private static final String STRING = "А ведь действительно,  правильно писать база акак? Многие авторы (и я тоже, иногда, в этом повинен) не много внимания уделяют правильности составления абзаца. Не говоря уже, о его размерах и структуре.\n"
             + "В прошлой статье, о структуре излагаемого материала, я вскользь упоминал об абзацах. Но, данной теме, стоит посвятить отдельную статью.";
 
-    private static final String OPERATION_REQUEST = "Please enter operation name: sortParagraph, sortWords, sortLexeme :";
-    private static final String SORT_PARAGRAPH = "sortParagraph";
-    private static final String SORT_WORDS = "sortWords";
-    private static final String SORT_LEXEME = "sortLexeme";
+    private static final String OPERATION_REQUEST = """
+            Please select number an operation :
+            1-> sort paragraphs by number of sentences;
+            2-> sort words by length in sentences;
+            3-> sort lexemes by occurrence of symbol;
+            4-> exit the application.
+            Your choice:\s""";
     private static final String WRONG_INPUT = "Wrong input, try again!";
 
     public static void main(String[] args) {
-        runApp(STRING);
+        runApp();
     }
 
-    private static void runApp(String string) {
-        String choice = inputString(OPERATION_REQUEST);
+    private static void runApp() {
+        int choice = inputIntValue(OPERATION_REQUEST);
+
         switch (choice) {
-            case (SORT_PARAGRAPH) -> sortParagraphsByNumberOfSentences(string);
-            case (SORT_WORDS) -> sortWordsInSentenceByLength(string);
-            case (SORT_LEXEME) -> sortLexemeInCurrentSentence(string);
+            case (1) -> sortParagraphsByNumberOfSentences(STRING);
+            case (2) -> sortWordsInSentenceByLength(STRING);
+            case (3) -> sortLexemeInCurrentSentence(STRING);
+            case (4) -> System.out.println("Application closed");
+
             default -> {
                 System.out.println(WRONG_INPUT);
-                runApp(STRING);
+                runApp();
             }
-
         }
     }
 }
--- src/com/epam/task/regex/Task2.java
+++ src/com/epam/task/regex/Task2.java
@@ -1,37 +1,46 @@
 package com.epam.task.regex;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Task2 {
-    private static final String OPEN_TAG = "(<[^/].*?[^/]>)";
-    private static final String CLOSED_TAG = "<\\/.+?>";
-    private static final String BODY = ">(.+?)</";
+    private static final String OPENING_TAG = "(<[^/].*?[^/]>)";
+    private static final String END_TAG = "</.+?>";
+    private static final String TAG_CONTENT = ">(.+?)</";
     private static final String EMPTY = "<\\w.+?/>";
+    private static final String RESULT_IS_OPENING_TAG = " - opening tag.\n";
+    private static final String RESULT_IS_TAG_CONTENT = " - it is tag content.\n";
+    private static final String RESULT_IS_END_TAG = " - end tag.\n";
+    private static final String RESULT_IS_EMPTY_TAG = " - empty.\n";
 
     private static final String XML =
-            "<notes>\n" +
-                    "   <note id = \"1\">\n" +
-                    "       <to>Вася</to>\n" +
-                    "       <from>Света</from>\n" +
-                    "       <heading>Напоминание</heading>\n" +
-                    "       <body>Позвони мне завтра!</body>\n" +
-                    "   </note>\n" +
-                    "   <note id = \"2\">\n" +
-                    "       <to>Петя</to>\n" +
-                    "       <from>Маша</from>\n" +
-                    "       <heading>Важное напоминание</heading>\n" +
-                    "       <body/>\n" +
-                    "   </note>\n" +
-                    "</notes>";
+            """
+                    <notes>
+                       <note id = "1">
+                           <to>Вася</to>
+                           <from>Света</from>
+                           <heading>Напоминание</heading>
+                           <body>Позвони мне завтра!</body>
+                       </note>
+                       <note id = "2">
+                           <to>Петя</to>
+                           <from>Маша</from>
+                           <heading>Важное напоминание</heading>
+                           <body/>
+                       </note>
+                    </notes>""";
 
-    public static String analyzeStringAsXML(String string) {
-        Pattern openTag = Pattern.compile(OPEN_TAG);
-        Pattern closedTag = Pattern.compile(CLOSED_TAG);
-        Pattern body = Pattern.compile(BODY);
+    public static void main(String[] args) {
+        String analysisResult = analyzeStringAsXML(XML);
+        System.out.println(analysisResult);
+    }
+
+    private static String analyzeStringAsXML(String string) {
+        Pattern openTag = Pattern.compile(OPENING_TAG);
+        Pattern closedTag = Pattern.compile(END_TAG);
+        Pattern body = Pattern.compile(TAG_CONTENT);
         Pattern emptyTag = Pattern.compile(EMPTY);
-        StringBuffer stringBuffer = new StringBuffer();
+        StringBuilder stringBuffer = new StringBuilder();
 
         String[] strings = string.split("\n\\s*");
 
@@ -41,20 +50,18 @@ public class Task2 {
             Matcher bodyMatcher = body.matcher(line);
             Matcher emptyMatcher = emptyTag.matcher(line);
             if (openMatcher.find()) {
-                stringBuffer.append(openMatcher.group()).append(" - open Tag.\n");
-            } else if (closedMatcher.find()) {
-                stringBuffer.append(closedMatcher.group()).append(" - closed Tag.\n");
-            } else if (bodyMatcher.find()) {
-                stringBuffer.append(bodyMatcher.group(1)).append(" - it is body.\n");
-            } else if (emptyMatcher.find()) {
-                stringBuffer.append(emptyMatcher.group()).append(" - empty.\n");
+                stringBuffer.append(openMatcher.group()).append(RESULT_IS_OPENING_TAG);
+            }
+            if (bodyMatcher.find()) {
+                stringBuffer.append(bodyMatcher.group(1)).append(RESULT_IS_TAG_CONTENT);
+            }
+            if (closedMatcher.find()) {
+                stringBuffer.append(closedMatcher.group()).append(RESULT_IS_END_TAG);
+            }
+            if (emptyMatcher.find()) {
+                stringBuffer.append(emptyMatcher.group()).append(RESULT_IS_EMPTY_TAG);
             }
         }
         return stringBuffer.toString();
-    }
-
-    public static void main(String[] args) {
-        String analyseResult = analyzeStringAsXML(XML);
-        System.out.println(analyseResult);
     }
 }
